4340657d024b60754cb5b26a308c0960dc55aebb
Nisarg Dharia <nisargdharia@gmail.com>
2021-01-26 22:27:57 -0500

    Added a scoring system with high scores

--- client/src/components/modules/GameBoard.js
+++ client/src/components/modules/GameBoard.js
@@ -4,12 +4,15 @@ import Opponent from "./Opponent.js";
 
 import "../../utilities.css";
 import "./GameBoard.css";
+import { get, post } from "../../utilities.js";
 
 class GameBoard extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      high_score: 0,
+      curr_coins: 0,
       board_height: 0,
       board_width: 0,
       player_x: 10,
@@ -17,6 +20,7 @@ class GameBoard extends Component {
       cpu_x: 100,
       cpu_y: 100,
       num_turns: 100,
+      curr_score: 0,
       up: false,
       down: false,
       left: false,
@@ -26,6 +30,8 @@ class GameBoard extends Component {
   }
 
   componentDidMount() {
+    this.getProfile();
+
     window.addEventListener('keydown', (event) => {
       if (event.key === "ArrowUp") {
         this.setState({up: true});
@@ -53,6 +59,17 @@ class GameBoard extends Component {
     this.setupGame();
   }
 
+  getProfile = () => {
+    get('/api/profile/fill', {
+      userId: this.props.userId,
+    }).then((profile) => {
+      this.setState({
+        curr_coins: profile.coins,
+        high_score: profile.high_score,
+      });
+    });
+  }
+
   setupGame = () => {
     let height = document.getElementById("Board").clientHeight;
     let width = document.getElementById("Board").clientWidth;
@@ -71,6 +88,7 @@ class GameBoard extends Component {
       down: false,
       player_interval: null,
       opponent_interval: null,
+      score_interval: null,
     }, () => {
       document.getElementById("Player").style.left = this.state.player_x + "px";
       document.getElementById("CPU").style.left = this.state.cpu_x + "px";
@@ -81,23 +99,35 @@ class GameBoard extends Component {
 
   playGame = () => {
     if (this.state.playing) {
+      if (this.state.curr_score > this.state.high_score) {
+        this.updateHighScore(this.state.curr_score);
+        this.setState({
+          high_score: this.state.curr_score,
+        });
+      }
+
       clearInterval(this.state.player_interval);
       clearInterval(this.state.opponent_interval);
+      clearInterval(this.state.score_interval);
       this.setupGame();
 
       this.setState({ 
         playing: false,
         player_interval: null,
         opponent_interval: null,
+        score_interval: null,
+        curr_score: 0,
       });
     } else {
       let player_moving = setInterval(this.moveOpponent, 30);
       let opponent_moving = setInterval(this.movePlayer, 30);
+      let score_counting = setInterval(this.increaseScore, 60);
 
       this.setState({ 
         playing: true,
         player_interval: player_moving,
         opponent_interval: opponent_moving,
+        score_interval: score_counting,
       });
     }
   }
@@ -159,6 +189,20 @@ class GameBoard extends Component {
     })
   };
 
+  increaseScore = () => {
+    let rate = Math.floor(this.state.num_turns / 500 + 1);
+    this.setState({
+      curr_score: this.state.curr_score + rate,
+    });
+  }
+
+  updateHighScore = (score) => {
+    post("/api/profile/score", {
+      userId: this.props.userId,
+      high_score: score,
+    });
+  }
+
   checkFinished = () => {
     if (Math.abs(this.state.player_x - this.state.cpu_x) <= 14 && Math.abs(this.state.player_y - this.state.cpu_y) <= 14) {
       this.playGame();
@@ -172,10 +216,14 @@ class GameBoard extends Component {
           <Player/>
           <Opponent/>
         </div>
-        <div id="Start" className="GameBoard-Button" onClick={this.playGame}>
-          <h3 className="GameBoard-Text">
-            {(this.state.playing === false) ? "Start New Game" : "End Game"}
-          </h3>
+        <div className="GameBoard-Bottom">
+          <h3 className="GameBoard-Score"> Current Score: {this.state.curr_score} </h3>
+          <div id="Start" className="GameBoard-Button" onClick={this.playGame}>
+            <h3 className="GameBoard-Text">
+              {(this.state.playing === false) ? "Start New Game" : "End Game"}
+            </h3>
+          </div>
+          <h3 className="GameBoard-Score"> High Score: {this.state.high_score} </h3>
         </div>
       </div>
     );
